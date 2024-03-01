0c9622280075f84dc12618c9aba699aeec20de4a
PavelUd <paveludin4@gmail.com>
2024-03-01 22:08:48 +0500

    commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import FilterView from './view/filter-view.js';
+import TripInfoView from './view/trip-info-view.js';
+import {render, RenderPosition} from './render.js';
+import BoardPresenter from './board-presenter.js';
+
+const siteMainContainer = document.querySelector('.trip-main');
+const filterContainer = document.querySelector('.trip-controls__filters');
+const tripEventsContainer = document.querySelector('.trip-events');
+
+console.log(siteMainContainer);
+
+const boardPresenter = new BoardPresenter({
+  container: tripEventsContainer
+});
+
+render(new TripInfoView(), siteMainContainer, RenderPosition.AFTERBEGIN);
+render(new FilterView(), filterContainer);
+
+boardPresenter.init();
